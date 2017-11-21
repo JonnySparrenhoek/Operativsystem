f76d73daa6174765eab6a713e0e5b5bf12ea8e9e
JonnySparrenhoek <JonnySp@kth.se>
2017-11-21 21:58:13 +0100

    Project part 2 done

---
+++ project/src/lib.rs
@@ -0,0 +1,23 @@
+#![feature(lang_items)]
+#![feature(const_fn)]
+#![feature(unique)]
+#![feature(const_unique_new)]
+#![no_std]
+
+extern crate rlibc;
+extern crate volatile;
+extern crate spin;
+
+#[macro_use]
+mod vga_buffer;
+
+#[no_mangle]
+pub extern fn rust_main() {
+    vga_buffer::clear_screen();
+    println!("Hello World{}", "!");
+    println!("{}", { println!("inner"); "outer" });
+    loop{}
+}
+
+#[lang = "eh_personality"] extern fn eh_personality() {}
+#[lang = "panic_fmt"] #[no_mangle] pub extern fn panic_fmt() -> ! {loop{}}
